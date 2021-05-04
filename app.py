ed6f4e84815bce4c9745f28a8fe549761a91e04d
peekay21 <46897789+peekay21@users.noreply.github.com>
2021-05-04 20:16:54 +0530

    Change front-end view of car-price-prediction page.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+model= pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 
 
 @app.route('/', methods=['GET'])
@@ -62,7 +62,7 @@ def predict():
         if output < 0:
             return render_template('index.html', prediction_texts='Sorry you cannot sell this car.')
         else:
-            return render_template('index.html', prediction_texts="You can sell the car at {}".format(output))
+            return render_template('index.html', prediction_texts="You can sell the car at {} lakhs.".format(output))
     else:
         return render_template('index.html')
 
